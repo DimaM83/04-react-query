90e2d448ca5521bcfc956f64250dee133445114c
DimaM83 <150599187+DimaM83@users.noreply.github.com>
2025-06-06 07:20:31 +0200

    fix useMovies.ts

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,11 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchMovies, type SearchMoviesResponse } from '../services/movieService';
+import { fetchMovies } from '../services/movieService';
+import type { SearchMoviesResponse } from '../types/movie';
 
 export function useMovies(query: string, page: number) {
-  return useQuery<SearchMoviesResponse, Error>({
+  return useQuery<SearchMoviesResponse>({
     queryKey: ['movies', query, page],
     queryFn: () => fetchMovies(query, page),
     enabled: !!query,
-    staleTime: 1000 * 60 * 5, 
+    staleTime: 1000 * 60 * 5,
+    placeholderData: (prev) => prev,
   });
 }
